2a5f16082ce58f4c8ee0de3d0613249813e0667d
Karolin Varner <karo@cupdev.net>
2013-04-02 00:13:38 +0200

    Parse options

--- pdfbrochure.sh
+++ pdfbrochure.sh
@@ -5,22 +5,103 @@
 function inc() { (( $1 = ${!1} + 1)); }
 function dec() { (( $1 = ${!1} - 1)); }
 
+### HELP ###########################################################
+
+usage() {
+  (                                     
+    echo "$@"                          
+    echo                                
+    (test "$USAGE" && echo "$USAGE")    \
+    || cat "$(dirname $0)/README.md"    \
+        2>/dev/null                     \
+    || man "$(basename "$0")"           
+  ) 1>&2
+  exit 1
+}
+
 ### VARIABLES ######################################################
 
-# Constants
+ARGV=( )
+
+setpvo() {
+  pvO=( $@ )
+  test ${#pvO[@]} -gt 1 || usage "There must be at least two pages"
+}
+
+setNpvo() { 
+  pvO=( $(seq "$1") )
+}
+
+setfile() {
+  test -x "$1" -o -n "$justtransform" || usage "Given file does not exist"
+  file="$1"
+}
+
+# OPTIONS -----------------
+
+while getopts 'n:s:f:te' OPTION; do
+  case $OPTION in
+    'e')
+      lastempty=true
+      ;;
+
+    't')
+      justtransform=true
+      ;;
+
+    'f')
+      file="$OPTARG"
+      ;;
+
+    'n')
+      setNpvo "$OPTARG"
+      ;;
+
+    's')
+      setpvo "$OPTARG"
+      ;; 
+      
+    'h')
+      usage
+      ;;
+
+    ?)
+      usage "Unknown option"
+      ;;
+
+    -)
+      ARGV+=( "$OPTARG" )
+      ;;
+  esac
+done
+
+# ARGUMENTS --------------------
+# Rest of the args
+shift $((OPTIND-1))
+ARGV+=( $@ )
+
+# ARG1 pvO
+if ! [ $pvO ]; then
+  setNpvo "$1"
+  shift
+fi
+
+# ARG2
+if [ ! $file ]; then
+  setfile "$1"
+  shift
+fi
+
+test "$@" && usage "Too many arguments!"
+
+# CONSTANTS ----------------------
+
 empty="{}"
-
-# ARGS
-appempty=true
-
-# Page vector/contains all pages sequentially; destination vector
-pvO=( $(seq $1) )
-pvN=( )
 
 ### FILL VECTOR ####################################################
 
 # Append empty last page
-test $appempty && pvO+=( "$empty" )
+test $lastempty && pvO+=( "$empty" )
 
 # Append empty pages until len is a multiple of 4
 ((app= 4 - ${#pvO[@]} % 4)) # Get required additional pages
@@ -34,6 +115,7 @@ done
 # Generate new pvOector
 i=0
 ((k=${#pvO[@]}-1))
+pvN=( )
 while [ $k -gt $i ]; do
     pvN+=( ${pvO[$k]} )
     pvN+=( ${pvO[$i]} )
@@ -47,4 +129,8 @@ done
 
 ### OUTPUT #############################################################
 
-echo ${pvN[@]}
+if [ $justtransform ]; then
+  echo ${pvN[@]}
+else
+  echo pdfjam --twoside --suffix book --nup '2x1' --landscape -- "$file" ${pvN[@]}
+fi
